f03a3cc9a3bcc108650b3fb8160a192dae85e39e
ogupta261 <53042654+ogupta261@users.noreply.github.com>
2023-12-12 20:50:01 +0530

    Initializing!

---
+++ src/main/java/com/assignment/gamecrud/GameCrudApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.gamecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameCrudApplication.class, args);
+	}
+
+}
