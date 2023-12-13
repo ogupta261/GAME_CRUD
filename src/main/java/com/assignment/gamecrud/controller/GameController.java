97b6677aa233761b20c23c990751d28fd53182a9
ogupta261 <53042654+ogupta261@users.noreply.github.com>
2023-12-13 06:29:13 +0530

    Finalizing GAME CRUD!

--- src/main/java/com/assignment/gamecrud/controller/GameController.java
+++ src/main/java/com/assignment/gamecrud/controller/GameController.java
@@ -22,26 +22,31 @@ public class GameController {
     }
 
     @PostMapping(value = "/createGame", consumes = "application/json", produces = "application/json")
+    @ResponseBody
     public ResponseEntity<GameDTO> createSingleGame(@RequestBody GameDTO newGame) {
         return new ResponseEntity<>(gameService.createGame(newGame), HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/getGame")
+    @ResponseBody
     public ResponseEntity<GameDTO> getGameByID(@RequestParam long id) {
         return ResponseEntity.ok(gameService.readGame(id));
     }
 
     @GetMapping(value = "/getAllGames")
+    @ResponseBody
     public ResponseEntity<ArrayList<GameDTO>> getAllGames() {
         return ResponseEntity.ok(gameService.getAllGames());
     }
 
     @PutMapping(value = "/updateGame")
+    @ResponseBody
     public ResponseEntity<GameDTO> updateGame(@RequestParam long id, @RequestBody GameDTO game) {
         return ResponseEntity.ok(gameService.updateGame(id,game));
     }
 
     @DeleteMapping(value = "/deleteGame")
+    @ResponseBody
     public ResponseEntity<String> deleteGame(@RequestParam long id) {
         gameService.deleteGame(id);
         return new ResponseEntity<>("Game deleted successfully", HttpStatus.OK);
